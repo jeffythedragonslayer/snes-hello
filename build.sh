dfc62e6224b40957a9f5105aa5e9ae8b1b49d66c
Jeff Linahan <jeff.linahan@gmail.com>
2022-06-02 23:58:22 -0400

    generates a .dbg file that Mesen-SX imports zero symbols from

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cl65 -C smc.cfg -o hello.smc -l hello.list -m hello.mmap hello.asm
+cl65 -C smc.cfg -o hello.smc -l hello.list -m hello.mmap hello.asm -Wl "--dbgfile" -Wl "hereitis.dbg"
